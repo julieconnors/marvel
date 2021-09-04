c73239b8bef7272aae21cfbd04861dd61f16a1ea
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-09-04 10:51:17 -0400

    first commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Julie Connors on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let charactersToFetch = "https://gateway.marvel.com:443/v1/public/characters?ts=marvel&apikey=4b52992ce24c7f32bd54bdabc7717050&hash=f173d7550279f53565b7fc469faa5be9"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
