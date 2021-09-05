4140b0ee20e1fb7e17b95e96a6ed3760aa7c0d10
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-09-05 07:43:02 -0400

    create image cache to prefetch images

--- Marvel/ImageCache.swift
+++ Marvel/ImageCache.swift
@@ -5,4 +5,23 @@
 //  Created by Julie Connors on 9/4/21.
 //
 
-import Foundation
+import UIKit
+
+class ImageCache {
+    
+    static let shared = ImageCache()
+    
+    private init() {}
+    
+    var cache: NSCache<NSString, UIImage> = NSCache()
+    
+    func findImage(string: String) -> UIImage? {
+        let nsImageString = string as NSString
+        return cache.object(forKey: nsImageString)
+    }
+    
+    func addImage(string: String, image: UIImage) {
+        let nsImageString = string as NSString
+        cache.setObject(image, forKey: nsImageString)
+    }
+}
